819dfb3158f93d119d9c0f2f3d5a2fa1a8431993
htim <timur.hudyakov@gmail.com>
2017-05-23 23:49:55 +0400

    fix

--- src/main/main.go
+++ src/main/main.go
@@ -2,7 +2,7 @@ package main
 
 func main()  {
 	a := App{}
-	a.Initialize("postgres://mjaovdiv:xxiD03VhsirEPIc-bX171XXchv_3vQhb@horton.elephantsql.com:5432/mjaovdiv")
+	a.Initialize()
 	a.Run(":3333")
 }
 
